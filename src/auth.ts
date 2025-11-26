8c19d3fccb22c595ba8a17168e82d19b46cb7105
amanrj111 <156581242+amanrj111@users.noreply.github.com>
2025-11-26 18:16:54 +0530

    fronted

--- src/auth.ts
+++ src/auth.ts
@@ -59,8 +59,21 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session
     },
 
-    
-  }
+
+  },
+
+  pages: {
+    signIn: "/login",
+    error: " /login"
+  },
+
+  session: {
+    strategy: "jwt",
+    maxAge: 10*24*60*60*1000
+  },
+
+  secret: process.env.AUTH_SCERET
+  
 });
 
 //connect to db
