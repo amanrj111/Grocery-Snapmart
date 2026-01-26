d623baa4bcc6d949e010081471ba31291df678de
amanrj111 <156581242+amanrj111@users.noreply.github.com>
2026-01-26 19:20:41 +0530

    userDashboard

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import CategorySllider from './CategorySllider'
 
 function UserDashboard() {
   return (
     <>
     <HeroSection/>
+    <CategorySllider/>
     </>
   )
 }
