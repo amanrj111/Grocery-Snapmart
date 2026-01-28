62990807d6a3aee3ec1a8c6c622f6dd4ce20403d
amanrj111 <156581242+amanrj111@users.noreply.github.com>
2026-01-28 22:23:25 +0530

    userDashb

--- src/components/GroceryItemCard.tsx
+++ src/components/GroceryItemCard.tsx
@@ -3,6 +3,7 @@
 import mongoose from "mongoose";
 import React from "react";
 import { motion } from "motion/react";
+import Image from "next/image";
 
 interface IGrocery {
   _id?: mongoose.Types.ObjectId;
@@ -17,13 +18,22 @@ interface IGrocery {
 function GroceryItemCard({ item }: { item: IGrocery }) {
   return (
     <motion.div
-    initial={{opacity:0,y:50,scale:0.9}}
-    whileInView={{opacity:1,y:0,scale:1}}
-    transition={{duration:0.6}}
-    viewport={{once:false,amount:0.3}}
-    className='bg-white rounded-2xl shadow-sm hover:shadow-xl transition-all duration-300 overflow-hidden border border-gray-100 flex flex-col'
+      initial={{ opacity: 0, y: 50, scale: 0.9 }}
+      whileInView={{ opacity: 1, y: 0, scale: 1 }}
+      transition={{ duration: 0.6 }}
+      viewport={{ once: false, amount: 0.3 }}
+      className="bg-white rounded-2xl shadow-sm hover:shadow-xl transition-all duration-300 overflow-hidden border border-gray-100 flex flex-col"
     >
-      
+      <div className="relative w-full aspect-4/3 bg-gray-50 overflow-hidden group">
+        <Image
+          src={item.image}
+          fill
+          alt={item.name}
+          sizes="(max-width: 768px) 100vw, 25vw"
+          className="object-contain p-4 transition-transform duration-500 group-hover:scale-105"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-black/10 to-transparent opacity-0 group-hover:opacity-100 transition-all duration-300" />
+      </div>
     </motion.div>
   );
 }
