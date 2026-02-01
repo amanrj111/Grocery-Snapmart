974a1b7551cc6c3f34fba95c92e783d618374384
amanrj111 <156581242+amanrj111@users.noreply.github.com>
2026-02-01 20:10:58 +0530

    GroceryItemCard

---
+++ src/hooks/useGetMe.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { AppDispatch } from "@/redux/store";
+import { setUserData } from "@/redux/userSlice";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, UseDispatch } from "react-redux";
+
+function useGetMe(enabled: boolean) {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    if (!enabled) return; // ✅ SAFE
+
+    let cancelled = false;
+
+    const getMe = async () => {
+      try {
+        const result = await axios.get("/api/me");
+        if (!cancelled) {
+          dispatch(setUserData(result.data));
+        }
+      } catch (error) {
+        console.error("GET ME FAILED:", error);
+      }
+    };
+    getMe();
+
+    return () => {
+      cancelled = true;
+    };
+  }, [enabled, dispatch]);
+}
+
+export default useGetMe;
